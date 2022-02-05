59010c2edd155e5eaf28c5bf848ea6671785cddd
somsekhardash <dashsomsekhar@gmail.com>
2022-02-05 11:47:19 +0530

    tooltip fix

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 window.flag = false;
 
-function popupDiv(url) {
+function popupDiv(url, min=8) {
     return `
         <div class="popup-wrapper" onmouseover="iframeFn(this, '`+ url + `')">
             <svg xmlns="http://www.w3.org/2000/svg" fill="#0072b3" width="12" height="12" viewBox="0 0 24 24"><path d="M12 1c-6.338 0-12 4.226-12 10.007 0 2.05.739 4.063 2.047 5.625.055 1.83-1.023 4.456-1.993 6.368 2.602-.47 6.301-1.508 7.978-2.536 9.236 2.247 15.968-3.405 15.968-9.457 0-5.812-5.701-10.007-12-10.007zm1 15h-2v-6h2v6zm-1-7.75c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"/></svg>
@@ -9,8 +9,8 @@ function popupDiv(url) {
                     
                 </div>
                 <div class="footer">
-                    <button class="bookmark" onclick="bookMark()">bookmark</button>
-                    <span class="read-time">8 Min</span>
+                    <span class="read-time">`+min+` Min</span>
+                    <svg class="bookmark" onclick="bookMark()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#0072b3" viewBox="0 0 24 24"><path d="M22 3v21h-20v-21h4.667l-2.667 2.808v16.192h16v-16.192l-2.609-2.808h4.609zm-3.646 4l-3.312-3.569v-.41c.001-1.668-1.352-3.021-3.021-3.021-1.667 0-3.021 1.332-3.021 3l.001.431-3.298 3.569h12.651zm-6.354-5c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm-5 15h10v1h-10v-1zm0-1h10v-1h-10v1zm0-2h10v-1h-10v1zm0-2h10v-1h-10v1z"/></svg>
                 </div>
             </div>    
         </div>`;
