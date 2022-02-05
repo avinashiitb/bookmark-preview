85daea08aac4b7624cb3f5c241125f1e3c29d888
somsekhardash <dashsomsekhar@gmail.com>
2022-02-05 11:28:18 +0530

    tooltip fix

--- content.js
+++ content.js
@@ -91,7 +91,7 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "start" && !window.flag) {
             window.flag = true;
-            gotMessage();
+            gotMessage(request);
         }
     }
 );
