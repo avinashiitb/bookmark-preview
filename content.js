3fe42f56e85bd1cd6d5134dc89b576e255c70443
somsekhardash <dashsomsekhar@gmail.com>
2022-02-09 13:43:53 +0530

    added bookmark button

--- content.js
+++ content.js
@@ -5,6 +5,12 @@ function popupDiv(url, min = Math.floor(Math.random() * 10)) {
         <div class="popup-wrapper" onmouseover="iframeFn(this, '`+ url + `')">
             <svg xmlns="http://www.w3.org/2000/svg" fill="#0072b3" width="12" height="12" viewBox="0 0 24 24"><path d="M12 1c-6.338 0-12 4.226-12 10.007 0 2.05.739 4.063 2.047 5.625.055 1.83-1.023 4.456-1.993 6.368 2.602-.47 6.301-1.508 7.978-2.536 9.236 2.247 15.968-3.405 15.968-9.457 0-5.812-5.701-10.007-12-10.007zm1 15h-2v-6h2v6zm-1-7.75c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"/></svg>
             <div class="popup">
+                <div class="footer">
+                    <span class="read-time">`+ min + ` Min</span>
+                    <button class="ui button tiny primary bookmark-button-call">
+                        Save
+                    </button>
+                </div>
                 <div id="header" class="header">
                     <div class="ui segment">
                         <div class="ui active inverted dimmer">
@@ -14,13 +20,7 @@ function popupDiv(url, min = Math.floor(Math.random() * 10)) {
                         <p></p>
                         <p></p>
                     </div>
-                </div>
-                <div class="footer">
-                    <span class="read-time">`+ min + ` Min</span>
-                    <div class="ui icon buttons">
-                        <svg class="bookmark" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#0072b3" viewBox="0 0 24 24"><path d="M22 3v21h-20v-21h4.667l-2.667 2.808v16.192h16v-16.192l-2.609-2.808h4.609zm-3.646 4l-3.312-3.569v-.41c.001-1.668-1.352-3.021-3.021-3.021-1.667 0-3.021 1.332-3.021 3l.001.431-3.298 3.569h12.651zm-6.354-5c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm-5 15h10v1h-10v-1zm0-1h10v-1h-10v1zm0-2h10v-1h-10v1zm0-2h10v-1h-10v1z"/></svg>
-                    </div>
-                </div>
+                </div>  
             </div>    
         </div>`;
 }
@@ -102,6 +102,7 @@ window.addEventListener("message", function (event) {
     //         //     });
     //         // });
     //     }
+
     if (event.source != window)
         return;
 
@@ -110,6 +111,7 @@ window.addEventListener("message", function (event) {
         async: true,
         success: function (data) {
             // console.log(data);
+            debugger;
             let bookPageTitle = data.match(/<title>(.*?)<\/title>/)[1];
             let shortcutIcon = document.querySelector("link[rel~='icon']") ? document.querySelector("link[rel~='icon']").href : '';
             const childData = {
@@ -183,8 +185,11 @@ function gotMessage(message, sender, sendresponse) {
             debugger;
             var data = { type: "url", value: e.currentTarget.href };
            
-            if(e.target.className==="header") {
-                window.postMessage(data, "*");
+            if($(e.target).hasClass("bookmark-button-call")) {
+                window.postMessage(data, "*"); 
+                e.target.innerHTML = "Done"
+                $(e.target).removeClass("primary");
+                $(e.target).addClass("green");
                 return false;
             }
            
@@ -221,6 +226,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "start" && !window.flag) {
             window.flag = true;
+            $(document.body).addClass('the-book-mark');
             gotMessage(request);
         }
     }
@@ -235,37 +241,20 @@ function getSelectionText() {
 }
 
 window.addEventListener('mouseup', function () {
-    let thetext = getSelectionText();
-    let shortcutIcon = document.querySelector("link[rel~='icon']") ? document.querySelector("link[rel~='icon']").href : '';
-    if (thetext.length > 0) { // check there's some text selected
-        // console.log(thetext, window.location) // logs whatever textual content the user has selected on the page
-        const childData = {
-            type: "text",
-            value: thetext,
-        }
-        chrome.storage.sync.get(["data"], function (result) {
-            console.log("Result", result.data);
-            storeData = result.data;
-            // First time data entry
-            if (!result.data) {
-                const parentData = {
-                    type: "url",
-                    value: window.location.href,
-                    parentTitle: document.title,
-                    parentFavicon: shortcutIcon,
-                    childNodes: [childData]
-                }
-                var rootData = {
-                    type: "parent",
-                    childNodes: [parentData]
-                };
-                chrome.storage.sync.set({ "data": rootData }, function () {
-                    console.log('Root Value is set to ', rootData);
-                });
+    if($(document.body).hasClass('the-book-mark')) {
+        let thetext = getSelectionText();
+        let shortcutIcon = document.querySelector("link[rel~='icon']") ? document.querySelector("link[rel~='icon']").href : '';
+        if (thetext.length > 0) { // check there's some text selected
+            // console.log(thetext, window.location) // logs whatever textual content the user has selected on the page
+            const childData = {
+                type: "text",
+                value: thetext,
             }
-            else if (storeData.childNodes && storeData.childNodes.length) {
-                const searcResult = search(storeData, childData, window.location.href);
-                if (!searcResult) {
+            chrome.storage.sync.get(["data"], function (result) {
+                console.log("Result", result.data);
+                storeData = result.data;
+                // First time data entry
+                if (!result.data) {
                     const parentData = {
                         type: "url",
                         value: window.location.href,
@@ -273,13 +262,32 @@ window.addEventListener('mouseup', function () {
                         parentFavicon: shortcutIcon,
                         childNodes: [childData]
                     }
-                    storeData.childNodes.push(parentData);
+                    var rootData = {
+                        type: "parent",
+                        childNodes: [parentData]
+                    };
+                    chrome.storage.sync.set({ "data": rootData }, function () {
+                        console.log('Root Value is set to ', rootData);
+                    });
                 }
-                console.log("searcResult", searcResult, window.location.href);
-                chrome.storage.sync.set({ "data": storeData }, function () {
-                    console.log('Value is set to ', storeData);
-                });
-            }
-        });
-    }    
+                else if (storeData.childNodes && storeData.childNodes.length) {
+                    const searcResult = search(storeData, childData, window.location.href);
+                    if (!searcResult) {
+                        const parentData = {
+                            type: "url",
+                            value: window.location.href,
+                            parentTitle: document.title,
+                            parentFavicon: shortcutIcon,
+                            childNodes: [childData]
+                        }
+                        storeData.childNodes.push(parentData);
+                    }
+                    console.log("searcResult", searcResult, window.location.href);
+                    chrome.storage.sync.set({ "data": storeData }, function () {
+                        console.log('Value is set to ', storeData);
+                    });
+                }
+            });
+        }    
+    }
 }, false)
